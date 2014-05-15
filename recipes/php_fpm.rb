843b219f20a6e62d3c7393c10edbb32c5380557a
Logan Koester <logan@logankoester.com>
2014-05-15 08:32:59 -0400

    Adds nginx::php_fpm recipe

---
+++ recipes/php_fpm.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: php_fm
+#
+# Copyright (C) 2014 Logan Koester
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+package('php') { action :install }
+package('php-fpm') { action :install }
+
+template '/etc/php/php.ini' do
+  mode '0644'
+  source 'php.ini.erb'
+end
+
+template '/etc/php/php-fpm.conf' do
+  mode '0644'
+  source 'php-fpm.conf.erb'
+end
+
+service 'php-fpm' do
+  provider Chef::Provider::Service::Systemd if node['platform'] =~ /arch|manjaro/
+  supports status: true, start: true, stop: true, restart: true, reload: true
+  action [:enable, :start]
+end
